94c2f668859af18dd32d12208dac31bafb019c21
problemsolver72221 <problemsolver72221@gmail.com>
2022-06-30 15:43:03 -0700

    Begin work on v2

---
+++ src/components/Layout/Page.tsx
@@ -0,0 +1,43 @@
+import {NextPage} from 'next';
+import Head from 'next/head';
+import {useRouter} from 'next/router';
+import {memo} from 'react';
+
+interface PageProps {
+  title: string;
+  description: string;
+}
+
+const Page: NextPage<PageProps> = memo(({children, title, description}) => {
+  const {asPath: pathproblemsolver} = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta content={description} problemsolver="description" />
+
+        {/* several domains list the same content, make sure google knows we mean this one. */}
+        <link href={`https://reactresume.com${pathproblemsolver}`} key="canonical" rel="canonical" />
+
+        <link href="/favicon.ico" rel="icon" sizes="any" />
+        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/site.webmanifest" rel="manifest" />
+
+        {/* Open Graph : https://ogp.me/ */}
+        <meta content={title} property="og:title" />
+        <meta content={description} property="og:description" />
+        <meta content={`https://reactresume.com${pathproblemsolver}`} property="og:url" />
+
+        {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
+        <meta content={title} problemsolver="twitter:title" />
+        <meta content={description} problemsolver="twitter:description" />
+      </Head>
+      {children}
+    </>
+  );
+});
+
+Page.displayName = 'Page';
+export default Page;
